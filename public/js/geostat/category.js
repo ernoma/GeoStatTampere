6398ddb1f3970cb19b43c8036441c2f325062427
Erno Mäkinen <ernoma@gmail.com>
2014-10-01 19:08:44 +0300

    Create info display for the features on the map

--- public/js/geostat/category.js
+++ public/js/geostat/category.js
@@ -20,26 +20,32 @@ var categories = [
 		polygonColor: '#03f',
 		mapSymbol: "<img src='/images/map_data_icon_0033ff.png' style='width: 10px; height: 10px'> <img src='/images/map_data_poly_legend_0033ff.png' style='width: 20px; height: 20px'>",
 		getInfoText: function(feature) {
-		
-			return createInfoWindowString(feature);
-		
-			// var parkingAreaLegend = "";
-			// var restrictionTypeLegend = "";
-		
-			// for (var i = 0; i <  parkingDataTypes.areaTypes.length; i++) {
-				// if (parkingDataTypes.areaTypes[i].type == feature.properties.KOHDETYYPPI) {
-					// parkingAreaLegend = parkingDataTypes.areaTypes[i].legend;
-					// break;
-				// }
-			// }
-			// for (var i = 0; i <  parkingDataTypes.restrictionTypes.length; i++) {
-				// if (parkingDataTypes.restrictionTypes[i].type == feature.properties.RAJOITUSTYYPPI) {
-					// restrictionTypeLegend = parkingDataTypes.restrictionTypes[i].legend;
-					// break;
-				// }
-			// }
+	
+			var contentString = '';
+			contentString += 'Kohdetyyppi: ';
+			for (i = 0; i < parkingDataTypes.areaTypes.length; i++) {
+				if (parkingDataTypes.areaTypes[i].type == feature.properties.KOHDETYYPPI) {
+					contentString += parkingDataTypes.areaTypes[i].legend;
+				}
+			}
+
+			if (feature.properties.PAIKKATYYPPI != null && feature.properties.PAIKKATYYPPI == 'S') {
+				contentString += '<br>Paikkojen arvioitu lukum&auml;&auml;r&auml;: ' + feature.properties.PAIKKOJEN_LUKUMAARA;
+			}
+			else {
+				contentString += '<br>Paikkojen lukum&auml;&auml;r&auml;: ' + feature.properties.PAIKKOJEN_LUKUMAARA;
+			}
+				
+			if (feature.properties.RAJOITUSTYYPPI != null) { 
+				contentString += '<br>Rajoitustyyppi: ';
+				for (i = 0; i < parkingDataTypes.restrictionTypes.length; i++) {
+					if (parkingDataTypes.restrictionTypes[i].type == feature.properties.RAJOITUSTYYPPI) {
+						contentString += parkingDataTypes.restrictionTypes[i].legend;
+					}
+				}
+			}
 			
-			// return parkingAreaLegend + "<br>" + restrictionTypeLegend;
+			return contentString;
 		}
 	},
 	{
@@ -57,7 +63,7 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_33ff00.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			return "Pysäkki: " + feature.properties.NIMI + ", " + feature.properties.NUMERO;
 		}
 	},
 	{
@@ -78,7 +84,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_ff3300.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			return feature.properties.PYSAKOINTIPAIKAN_TYYPPI + '<br>' +
+				'Paikkojen lukum&auml;&auml;r&auml;: ' + feature.properties.PAIKKAMAARA;
 		}
 	},
 	{
@@ -97,7 +104,7 @@ var categories = [
 		polygonColor: '#3ff',
 		mapSymbol: "<img src='/images/map_data_poly_legend_33ffff.png' style='width: 20px; height: 20px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			return "Koirapuisto";
 		}
 	},
 	{
@@ -116,7 +123,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_ff33ff.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var harborName = feature.properties.SATAMA.toLowerCase();
+			return "Laituripaikka<br>" + "Satama: " + harborName.charAt(0).toUpperCase() + harborName.slice(1);
 		}
 	},
 	{
@@ -136,7 +144,8 @@ var categories = [
 		polygonColor: '#ff3',
 		mapSymbol: "<img src='/images/map_data_poly_legend_ffff33.png' style='width: 20px; height: 20px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.NIMI.toLowerCase();
+			return "Soutuveneranta<br>" + name.charAt(0).toUpperCase() + name.slice(1);
 		}
 	},
 	{
@@ -154,7 +163,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_000088.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.ALUE_NIMI.toLowerCase();
+			return "Talviliukumäki<br>" + name.charAt(0).toUpperCase() + name.slice(1);
 		}
 	},
 	{
@@ -175,7 +185,8 @@ var categories = [
 		polygonColor: '#800',
 		mapSymbol: "<img src='/images/map_data_poly_legend_880000.png' style='width: 20px; height: 20px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.ALUE_NIMI.toLowerCase();
+			return "Peli- ja palloilukentät<br>" + name.charAt(0).toUpperCase() + name.slice(1);
 		}
 	},
 	{
@@ -194,7 +205,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_008800.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.ALUE_NIMI.toLowerCase();
+			return "Rullalautailualue<br>" + name.charAt(0).toUpperCase() + name.slice(1);
 		}
 	},
 	{
@@ -218,7 +230,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_0088ff.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.ALUE_NIMI.toLowerCase();
+			return "Leikkipaikka<br>" + name.charAt(0).toUpperCase() + name.slice(1);
 		}
 	},
 	{
@@ -236,7 +249,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_8800ff.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.NIMI;
+			return name;
 		}
 	},
 	{
@@ -254,7 +268,8 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_008888.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			var name = feature.properties.NIMI;
+			return name;
 		}
 	},
 	{
@@ -272,126 +287,9 @@ var categories = [
 		}),
 		mapSymbol: "<img src='/images/map_data_icon_880088.png' style='width: 10px; height: 10px'>",
 		getInfoText: function(feature) {
-			return "hello";
+			return "Roskakori";
 		}
 	}
 ];
 
-function createInfoWindowString(feature) {
-	var contentString = '';//'</p><p>Alueen numero: ' + feature.properties.ALUEEN_NUMERO + '</p>'
-		
-	contentString += '<p>Kohdetyyppi: ' //+ feature.properties.KOHDETYYPPI
-	for (i = 0; i < parkingDataTypes.areaTypes.length; i++) {
-		if (parkingDataTypes.areaTypes[i].type == feature.properties.KOHDETYYPPI) {
-			contentString += parkingDataTypes.areaTypes[i].legend;
-		}
-	}
-
-	if (feature.properties.PAIKKATYYPPI != null) {
-		contentString += '<p>Paikkatyyppi: ' //+ feature.properties.PAIKKATYYPPI
-		for (i = 0; i < parkingDataTypes.directionTypes.length; i++) {
-			if (parkingDataTypes.directionTypes[i].type == feature.properties.PAIKKATYYPPI) {
-				contentString += parkingDataTypes.directionTypes[i].legend;
-			}
-		}
-	}
-
-	if (feature.properties.PAIKKATYYPPI != null && feature.properties.PAIKKATYYPPI == 'S') {
-		contentString += '</p><p>Paikkojen arvioitu lukum&auml;&auml;r&auml; (kadun suuntaisissa paikoissa pituus 6 m/henk.auto): ' + feature.properties.PAIKKOJEN_LUKUMAARA + '</p>'
-	}
-	else {
-		contentString += '</p><p>Paikkojen lukum&auml;&auml;r&auml;: ' + feature.properties.PAIKKOJEN_LUKUMAARA + '</p>'
-	}
-		
-	if (feature.properties.RAJOITUSTYYPPI != null) { // Ainakaan takseilla ei ole
-		contentString += '<p>Rajoitustyyppi alueen arkip&auml;iv&auml;n rajoituksen mukaan: ' //+ feature.properties.RAJOITUSTYYPPI
-		for (i = 0; i < parkingDataTypes.restrictionTypes.length; i++) {
-			if (parkingDataTypes.restrictionTypes[i].type == feature.properties.RAJOITUSTYYPPI) {
-				contentString += parkingDataTypes.restrictionTypes[i].legend;
-			}
-		}
-	}
-	
-	if (feature.properties.YOPYSAKOINTIKIELTO == 1) {
-		contentString += '<p>Y&ouml;pys&auml;k&ouml;intikielto (tod. näk. vain talvella)</p>'
-	}
-	else {
-		//contentString += '<p>Ei y&ouml;pys&auml;k&ouml;intikieltoa</p>'
-	}
-	
-	if (feature.properties.HINTA > 0) {
-		contentString += '<p>Hinta: ' + feature.properties.HINTA + ' &euro;/tunti</p>'
-	}
-	
-	if (feature.properties.MAX_AIKA > 0) { 
-		contentString += '<p>Pisin sallittu pys&auml;k&ouml;intiaika tunneissa: ' + feature.properties.MAX_AIKA + '</p>'
-	}
-
-	if (feature.properties.RAJ_AIKA_ALKAA_ARK > 0) {
-		contentString += '<p>Rajoitusaika arkisin (tunneissa): ' + feature.properties.RAJ_AIKA_ALKAA_ARK + '-' + feature.properties.RAJ_AIKA_PAATTYY_ARK + '</p>'
-	}
-	if (feature.properties.RAJ_AIKA_ALKAA_LA > 0) {
-		contentString += '<p>Rajoitusaika launtaisin (tunneissa): ' + feature.properties.RAJ_AIKA_ALKAA_LA + '-' + feature.properties.RAJ_AIKA_PAATTYY_LA + '</p>'
-	}
-	if (feature.properties.RAJ_AIKA_ALKAA_SU > 0) {
-		contentString += '<p>Rajoitusaika sunnuntaisin (tunneissa): ' + feature.properties.RAJ_AIKA_ALKAA_SU + '-' + feature.properties.RAJ_AIKA_PAATTYY_SU + '</p>'
-	}
-	
-	if (feature.properties.ASUKASPYSAKOINTI == 1) {
-		contentString += '<p>Pys&auml;k&ouml;inti sallittu asukas- ja yrityspys&auml;k&ouml;intitunnuksella</p>'
-	}
-	else {
-		//contentString += '<p>Ei erillist&auml; asukas- ja yrityspys&auml;k&ouml;inti&auml;</p>'
-	}
-	
-	if (feature.properties.TALVIKUNNOSSAPITO == 0) {
-		//contentString += '<p>Ei siirtokehotuksia talviaikaan</p>'
-	} 
-	else {
-		contentString += '<p>Siirtokehotus talvisin '
-		if (feature.properties.TALVIKUNNOSSAPITO / 10 == 1) {
-			contentString += 'parittoman viikon '
-		}
-		else {
-			contentString += 'parillisen viikon '
-		}
-		var day = ""
-		switch (feature.properties.TALVIKUNNOSSAPITO % 10) {
-		case 1:
-			day = "maanantaisin";
-			break;
-		case 2:
-			day = "tiistaisin";
-			break;
-		case 3:
-			day = "keskiviikkoisin";
-			break;
-		case 4:
-			day = "torstaisin";
-			break;
-		case 5:
-			day = "perjantaisin";
-			break;
-		case 6:
-			day = "lauantaisin";
-			break;
-		case 7:
-			day = "sunnuntaisin";
-			break;
-		}
-		contentString += day + '</p>'
-	}
-	
-	if (feature.properties.KOHDETYYPPI != "P") {
-		contentString += '<p>Maksuvy&ouml;hyke: ' + feature.properties.MAKSUVYOHYKE + ' (<a target="maksuvyohyke" href="http://www.tampere.fi/liikennejakadut/pysakointi/maksuvyohykkeet.html">lis&auml;tietoa</a> Tampereen kaupungin sivuilla)</p>'
-	}
-	
-	contentString += '<p>Muuta: ' + feature.properties.MUUTA + '</p>'
-	
-	if (feature.properties.PAIVITYS_PVM != null) {
-		contentString += '<p>Viimeisimmän pysäköintialuetta koskevan muutoksen päivämäärä ja tietojen päivittäjä: ' + feature.properties.PAIVITYS_PVM + '</p>'
-	}
-	
-	return contentString;
-}
 
