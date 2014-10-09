7205140c8ff88bf91857c4ab7d3d5edbc28d2682
Erno Mäkinen <ernoma@gmail.com>
2014-10-09 21:24:53 +0300

    Add spinner to inform user of the data retrieval

--- public/js/geostat/geostat.js
+++ public/js/geostat/geostat.js
@@ -831,3 +831,35 @@ $("#geolocate_button").click(geoLocate);
 
 //map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
+
+var opts = {
+  lines: 10, // The number of lines to draw
+  length: 6, // The length of each line
+  width: 3, // The line thickness
+  radius: 6, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '0' // Left position relative to parent
+};
+var target = document.getElementById('spinner');
+var spinner = new Spinner(opts);
+//spinner.spin(target);
+
+$(document)
+  .ajaxStart(function () {
+    spinner.spin(target);
+	//console.log("spin");
+  })
+  .ajaxStop(function () {
+     spinner.stop();
+	 //console.log("all done");
+  });
